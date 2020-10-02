fe979e4e2eaf75d520d646ac00c757643a383a79
Farzad Sunavala <farzadsunavala@chevron.com>
2020-10-02 12:58:55 -0500

    added a header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from './Header'
 
 function App() {
   return (
-<div>
-  <h1> Let's build the TINDER CLone App</h1>
-</div>
+    <div>
+      <h1> Let's build the TINDER CLone App</h1>
+
+      {/* Header */}
+      <Header />
+      {/* Tinder Cards */}
+      {/* buttons below tinder cards */}
+
+      {/* Chats screen */}
+      {/* Individual chat screen */}
+
+    </div>
   );
 }
 
