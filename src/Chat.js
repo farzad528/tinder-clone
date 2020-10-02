c9ad75a4adb108206b982da85a230752c5b3bfb7
Farzad Sunavala <farzadsunavala@chevron.com>
2020-10-02 17:53:50 -0500

    chat screen

--- src/Chat.js
+++ src/Chat.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Avatar from "@material-ui/core/Avatar";
+import './Chat.css'
 
 function Chat({name, message, profilePic, timestamp}) {
   return (
     <div className="chat">
       <Avatar className="chat__image" src={profilePic} />
-      <div>
+      <div className="chat__details">
         <h2>{name}</h2>
         <p>{message}</p>
       </div>
-      <p>{timestamp}</p>
+      <p className="chat__timestamp">{timestamp}</p>
     </div>
   );
 }
